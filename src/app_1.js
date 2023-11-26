9e806507b6938ff2b70155a112b306ac3aaddabe
AndreoliChiara <chiara.andreoli.2002@gmail.com>
2023-11-26 18:42:04 +0100

    modifica layout

--- src/app_1.js
+++ src/app_1.js
@@ -1,3 +1,43 @@
+// let demo_button = document.getElementById("start_demo");
+// alert("load");
+// demo_button.addEventListener("click", () => {
+//     alert("test");
+    
+
+//     e.preventDefault();
+
+//     // Request permission for iOS 13+ devices
+//     if (
+//         DeviceMotionEvent &&
+//         typeof DeviceMotionEvent.requestPermission === "function"
+//     ) {
+//         DeviceMotionEvent.requestPermission();
+//     }
+
+//     if (is_running) {
+//         window.removeEventListener("devicemotion", handleMotion);
+//         window.removeEventListener("deviceorientation", handleOrientation);
+//         demo_button.innerHTML = "Start demo";
+//         demo_button.classList.add('btn-success');
+//         demo_button.classList.remove('btn-danger');
+//         is_running = false;
+//     } else {
+//         window.addEventListener("devicemotion", handleMotion);
+//         window.addEventListener("deviceorientation", handleOrientation);
+//         document.getElementById("start_demo").innerHTML = "Stop demo";
+//         demo_button.classList.remove('btn-success');
+//         demo_button.classList.add('btn-danger');
+//         is_running = true;
+//     }
+// })
+
+
+
+
+//Data corrente
+
+
+
 
 var dataCorrente = new Date();
 // Ottenere i componenti della data (giorno, mese, anno)
@@ -5,68 +45,49 @@ var giorno = dataCorrente.getDate();
 var mese = dataCorrente.getMonth() + 1; // Mesi iniziano da zero, quindi aggiungi 1
 var anno = dataCorrente.getFullYear();
 var ore = dataCorrente.getHours();
-        var minuti = dataCorrente.getMinutes();
-        var secondi = dataCorrente.getSeconds();
+var minuti = dataCorrente.getMinutes();
+var secondi = dataCorrente.getSeconds();
 
 var dataFormattata = anno + '-' + (mese < 10 ? '0' + mese : mese) + '-' + (giorno < 10 ? '0' + giorno : giorno) + ' ' +
-                             (ore < 10 ? '0' + ore : ore) + ':' + (minuti < 10 ? '0' + minuti : minuti) + ':' + (secondi < 10 ? '0' + secondi : secondi);
+    (ore < 10 ? '0' + ore : ore) + ':' + (minuti < 10 ? '0' + minuti : minuti) + ':' + (secondi < 10 ? '0' + secondi : secondi);
 
-        // Inserire la data nella pagina HTML
-        document.body.innerHTML += '<p>Data corrente: ' + dataFormattata + '</p>';
-
-
+// Inserire la data nella pagina HTML
+document.body.innerHTML += '<p>Data corrente: ' + dataFormattata + '</p>';
 
 
-
-
-
-
+//Domande
 const elevenLabsApiKey = '12a57bdb2e2c1ba623fc969c9ca50631';
 const button = document.querySelector('button');
 const clickToRecordButton = document.getElementById('click_to_record');
+const questionContainer = document.getElementById('question-container');
 
+const questions = [
+    "What emotions did you feel today?",
+    "Did something happen that affected your day?",
+    "What did you do during the day?",
+    "What was the best part and the worst part of the day?",
+    // Aggiungi altre domande se necessario
+];
 
-//Cliccando bottone appare e la domanda
-button.addEventListener("click", function () {
-    const questions = [
-        "What emotions did you feel today?",
-        // Aggiungi altre domande se necessario
-    ];
+let questionIndex = 0;
 
-    let questionIndex = 0;
-
-
-
-    //Velocità con cui appare la domanda 
-    function typeQuestion(question, element, index = 0) {
-        if (index < question.length) {
-            element.innerHTML += question.charAt(index);
-            index++;
-            setTimeout(() => typeQuestion(question, element, index), 120);
-        } 
+//Velocità con cui appare la domanda 
+function typeQuestion(question, element, index = 0) {
+    if (index < question.length) {
+        element.innerHTML += question.charAt(index);
+        index++;
+        setTimeout(() => typeQuestion(question, element, index), 70);
     }
+}
 
-    const question = questions[questionIndex];
-    const questionContainer = document.getElementById('question-container');
-
-    questionContainer.innerHTML = '';
-    typeQuestion(question, questionContainer);
-    speak(question);
-
-});
-
-
-
-
-//Voce che legge la domanda
-
-async function speak(question) {
+// Funzione per leggere una domanda dall'AI
+async function readQuestion(question) {
     const text = question;
     const voiceId = "I5ANhMcPbMpJJNCGKeAx";
 
     const headers = new Headers();
     headers.append("Accept", "audio/mpeg");
-    headers.append("xi-api-key", "12a57bdb2e2c1ba623fc969c9ca50631");
+    headers.append("xi-api-key", elevenLabsApiKey);
     headers.append("Content-Type", "application/json");
 
     const body = JSON.stringify({
@@ -77,8 +98,6 @@ async function speak(question) {
             similarity_boost: 0.5,
         },
     });
-
-    
 
     try {
         const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`, {
@@ -107,10 +126,25 @@ async function speak(question) {
     }
 }
 
+// Cliccando il pulsante appare la domanda
+button.addEventListener("click", function () {
+    // Verifica se ci sono ancora domande disponibili
+    if (questionIndex < questions.length) {
+        const question = questions[questionIndex];
+        questionContainer.innerHTML = '';
+        typeQuestion(question, questionContainer);
+        readQuestion(question);
+
+        // Incrementa l'indice della domanda corrente
+        questionIndex++;
+    }
+});
 
 
 
-//Speech to texts
+
+
+//Trasforma la voce in testo
 
 clickToRecordButton.addEventListener('click', function () {
     const speech = true;
@@ -121,94 +155,48 @@ clickToRecordButton.addEventListener('click', function () {
 
     recognition.addEventListener('result', e => {
         const transcript = Array.from(e.results)
-          .map(result => result[0])
-          .map(result => result.transcript)
-          .join('');
-    
-        document.getElementById("convert_text").value = transcript;
-        
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('');
+
+        document.getElementById("convert_text").textContent = transcript;
+
         // Aggiorna la variabile della risposta corrente
         rispostaCorrente = transcript;
-    
+
         console.log(transcript);
-      });
-    
+    });
 
 
 
+    // Salva risposta quando finisci di parlare.
 
-      // Salva risposta quando finisci di parlare.
-
-      recognition.addEventListener('end', function () {
+    recognition.addEventListener('end', function () {
         // Chiamata alla funzione per salvare la risposta solo quando l'utente ha finito di parlare
         salvaRisposta();
-      });
-    
-      if (speech == true) {
-        recognition.start();
-      }
     });
-    
-    // Funzione per salvare la risposta
-    function salvaRisposta() {
-      if (rispostaCorrente.trim() !== "") {
+
+    if (speech == true) {
+        recognition.start();
+    }
+});
+
+// Funzione per salvare la risposta
+function salvaRisposta() {
+    if (rispostaCorrente.trim() !== "") {
         // Aggiungi la risposta alla lista nella sezione "Archivio"
         const risposteArchivio = document.getElementById("risposte-archivio");
         const nuovaRisposta = document.createElement("li");
         nuovaRisposta.textContent = rispostaCorrente + " - " + dataFormattata; // Aggiungi la data alla risposta
         risposteArchivio.appendChild(nuovaRisposta);
-    
+
         // Resettare la variabile della risposta corrente dopo averla salvata
         rispostaCorrente = "";
-      }
     }
-
-
-// const answers = [];
-
-// function saveAnswer() {
-//     const answer = document.getElementById("convert_text").value;
-//     answers.push(answer);
-//     document.getElementById("convert_text").value = ""; // Pulisce il campo di input
-//     displayAnswers();
-// }
-
-// // Funzione per visualizzare le risposte salvate
-// function displayAnswers() {
-//     const answerContainer = document.getElementById("answer-container");
-//     answerContainer.innerHTML = "<h3>Answers:</h3>";
-//     answers.forEach((answer, index) => {
-//         answerContainer.innerHTML += `<p>${index + 1}. ${answer}</p>`;
-//     });
-// }
+}
 
 
 
 
 
 
-// if (window.DeviceMotionEvent) {
-//     window.addEventListener('devicemotion', function(event) {
-//         // Accelerazione lungo l'asse X
-//         var accelerationX = event.acceleration.x;
-
-//         // Accelerazione lungo l'asse Y
-//         var accelerationY = event.acceleration.y;
-
-//         // Accelerazione lungo l'asse Z
-//         var accelerationZ = event.acceleration.z;
-
-//         // Aggiorna i valori sulla pagina HTML
-//         document.getElementById('acceleration-x').textContent = 'Acceleration X: ' + accelerationX;
-//         document.getElementById('acceleration-y').textContent = 'Acceleration Y: ' + accelerationY;
-//         document.getElementById('acceleration-z').textContent = 'Acceleration Z: ' + accelerationZ;
-//     });
-// } else {
-//     // Il dispositivo non supporta l'accelerometro o l'API DeviceMotion
-//     alert("Il tuo dispositivo non supporta l'accelerometro o l'API DeviceMotion.");
-// }
-
-
-
-
-    
